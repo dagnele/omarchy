1dac107c08de3481955ecebdb1b6dda688fcbb68
Daniele Galdi <daniele.galdi@gmail.com>
2025-11-22 18:10:55 +0000

    Install lsof

--- 02-install-nvim.sh
+++ 02-install-nvim.sh
@@ -5,3 +5,8 @@ set -e # Exit on error
 echo "Installing Neovim configurations..."
 mkdir -p ~/.config/nvim
 stow -vv nvim
+
+# Install lsof required by the opencode plugin i am using
+echo "Installing lsof..."
+sudo pacman -Sy lsof
+
